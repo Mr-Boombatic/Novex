74dcf95b33f8ef6d057d419880b423fedd87bfbe
Alexandr Telminov <alektelminoff@gmail.com>
2024-03-02 22:38:55 +0300

    Changes to be committed: 	modified:   app/src/Controller/UserController.php

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -6,10 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\User;
 use OpenApi\Attributes as OA;
-
-use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 
 use App\Service\UserService;
 
@@ -31,7 +28,23 @@ class UserController extends AbstractController
         description: 'Ошибка на стороне сервера.',
     )]
     #[Route('/api/user-create', name: 'app_create', methods: ['POST'], format: 'json')]
-    public function createUser(#[MapQueryString] User $request, UserService $userService): JsonResponse
+    public function createUser(
+        #[OA\RequestBody(
+            required: true,
+            description: 'Новый пользователь',
+            content: new OA\JsonContent(
+                example: '{
+                    "email": "test@example.com",
+                    "name": "Test",
+                    "age": 35,
+                    "sex": "male",
+                    "birthday": "1992-05-15",
+                    "phone": "89261234567"
+                }'
+            )
+         )] 
+        Request $request, 
+        UserService $userService): JsonResponse
     {
         return $this->processUserRequest($request, $userService, 'create');
     }
@@ -52,16 +65,32 @@ class UserController extends AbstractController
         description: 'Ошибка на стороне сервера.',
     )]
     #[Route('/api/user-update', name: 'user-update', methods: ['POST'], format: 'json')]
-    public function updateUser(#[OA\RequestBody] Request $request, UserService $userService): JsonResponse
+    public function updateUser(
+        #[OA\RequestBody(
+            required: true,
+            description: 'Новый пользователь',
+            content: new OA\JsonContent(
+                example: '{
+                    "id": 1
+                    "email": "test@example.com",
+                    "name": "Test",
+                    "age": 35,
+                    "sex": "male",
+                    "birthday": "1992-05-15",
+                    "phone": "89261234567"
+                }'
+            )
+         )] 
+        Request $request, UserService $userService): JsonResponse
     {
         return $this->processUserRequest($request, $userService, 'update');
     }
 
-    private function processUserRequest(Request $user, UserService $userService, string $operation): JsonResponse
+    private function processUserRequest(Request $request, UserService $userService, string $operation): JsonResponse
     {
-
+        $userData = json_decode($request->getContent(), true);
         try {
-            $errors = $operation === 'update' ? $userService->updateUser($user) : $userService->saveUser($user); 
+            $errors = $operation === 'update' ? $userService->updateUser($userData) : $userService->saveUser($userData); 
             $response = ($errors !== null) ?  
                 $this->json(['message' => $errors], 400) :
                 $this->json(['message' => 'User is updated successfully'], 201);
