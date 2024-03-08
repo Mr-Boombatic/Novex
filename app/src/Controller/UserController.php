d6c24f22ae5735aa5223c4841688f9b6faa4ccc6
Alexandr Telminov <alektelminoff@gmail.com>
2024-03-08 13:05:00 +0300

    modified:   app/config/packages/nelmio_api_doc.yaml 	modified:   app/src/Controller/UserController.php

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -2,108 +2,82 @@
 
 namespace App\Controller;
 
-use ApiPlatform\OpenApi\Model\RequestBody;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Nelmio\ApiDocBundle\Annotation\Model;
-use App\Entity\User;
 use OpenApi\Attributes as OA;
 
 use App\Service\UserService;
-use OpenApi\Context;
 
 class UserController extends AbstractController
 {
     #[
-    OA\Post(
-        path: '/api/user-create',
-        summary: "Создание пользователя",
-        requestBody: new OA\RequestBody(
-            description: 'test',
-            content: new Model(type: User::class))),
-    OA\Response(
-        response: 200,
-        description: 'Пользователь создан успешно'),   
-    OA\Response(
-        response: 400,
-        description: 'Пользователь не был создан.'),
-    OA\Response(
-        response: 500,
-        description: 'Ошибка на стороне сервера.')    
-    ]
-         
-    #[Route('/api/user-create', name: 'app_create', methods: ['POST'], format: 'json')]
-    public function createUser(
-        Request $request, 
-        UserService $userService): JsonResponse
-    {
-        return $this->processUserRequest($request, $userService, 'create');
-    }
-
-    /**
-     * Изменение существующего пользователя.
-     */
-    #[OA\Response(
-        response: 200,
-        description: 'Пользователь создан успешно',
-    )]
-    #[OA\Response(
-        response: 400,
-        description: 'Пользователь не был создан.',
-    )]
-    #[OA\Response(
-        response: 500,
-        description: 'Ошибка на стороне сервера.',
-    )]
-    #[Route('/api/user-update', name: 'user-update', methods: ['POST'], format: 'json')]
-    public function updateUser(
-        #[OA\RequestBody(
-            required: true,
-            description: 'Новый пользователь',
-            content: new OA\JsonContent(
-                example: '{
-                    "id": 1,
+        OA\Post(
+            path: '/api/user-create',
+            description: "Создание пользователя",
+            requestBody: new OA\RequestBody(
+                content: new OA\JsonContent(
+                    ref: '#/components/schemas/CreatingUser',
+                    example: '{
                     "email": "test@example.com",
                     "name": "Test",
                     "age": 35,
                     "sex": "male",
                     "birthday": "1992-05-15",
                     "phone": "89261234567"
-                }',
-                schema: '{
-                    "type": "object",
-                    "properties": {
-                        "id": {
-                            "type": "integer"
-                        },
-                        "email": {
-                            "type": "string"
-                        },
-                        "name": {
-                            "type": "string"
-                        },
-                        "age": {
-                            "type": "integer"
-                        },
-                        "sex": {
-                            "type": "string",
-                            "enum": ["male", "female"]
-                        },
-                        "birthday": {
-                            "type": "string",
-                            "format": "date"
-                        },
-                        "phone": {
-                            "type": "string"
-                        }
-                    },
-                    "required": ["id", "email", "name", "age", "sex", "birthday", "phone"]
-                }'
+                }')
             )
-         )] 
-        Request $request, UserService $userService): JsonResponse
+        ),
+        OA\Response(
+            response: 200,
+            description: 'User is created successfully'),
+        OA\Response(
+            response: 400,
+            description: 'User isn\'t created'),
+        OA\Response(
+            response: 500,
+            description: 'Internal error')
+    ]
+
+    #[Route('/api/user-create', name: 'app_create', methods: ['POST'], format: 'json')]
+    public function createUser(
+        Request $request,
+        UserService $userService): JsonResponse
+    {
+        return $this->processUserRequest($request, $userService, 'create');
+    }
+
+    #[
+        OA\Post(
+            path: '/api/user-create',
+            description: "Создание пользователя",
+            requestBody: new OA\RequestBody(
+                content: new OA\JsonContent(
+                    ref: '#/components/schemas/UpdatingUser',
+                    example: '{
+                    "id": 1
+                    "email": "test@example.com",
+                    "name": "Test",
+                    "age": 35,
+                    "sex": "male",
+                    "birthday": "1992-05-15",
+                    "phone": "89261234567"
+                }')
+            )
+        ),
+        OA\Response(
+            response: 200,
+            description: 'User is updated successfully'),
+        OA\Response(
+            response: 400,
+            description: 'User isn\'t updated'),
+        OA\Response(
+            response: 500,
+            description: 'Internal error')
+    ]
+    #[Route('/api/user-update', name: 'user-update', methods: ['POST'], format: 'json')]
+    public function updateUser(Request $request, UserService $userService): JsonResponse
     {
         return $this->processUserRequest($request, $userService, 'update');
     }
@@ -112,8 +86,8 @@ class UserController extends AbstractController
     {
         $userData = json_decode($request->getContent(), true);
         try {
-            $errors = $operation === 'update' ? $userService->updateUser($userData) : $userService->saveUser($userData); 
-            $response = ($errors !== null) ?  
+            $errors = $operation === 'update' ? $userService->updateUser($userData) : $userService->saveUser($userData);
+            $response = ($errors !== null) ?
                 $this->json(['message' => $errors], 400) :
                 $this->json(['message' => 'User is updated successfully'], 201);
         } catch (\Exception $e) {
